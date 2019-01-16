24da0deba00541a40c2d81f6cb227f9ff4f1beb6
Bernardo SOUSA <bernardodesousa@gmail.com>
2019-01-16 11:02:58 -0200

    Replace hard-coded list of themes by dynamically generate one.

--- chapter/ui/index.js
+++ chapter/ui/index.js
@@ -6,6 +6,18 @@ import modify from './modify'
 import duplicate from './duplicate'
 import extractFromForm from './extract-from-form'
 
+// build list of themes based on string.material.themes array
+// console.log(string.material.themes);
+
+let themesList = "";
+let i, l;
+l = string.material.themes.length;
+for (i = 0; i < l; i++) {
+    themesList += `<option value="${i}">${string.material.themes[i].adjective}</option>`;
+}
+
+document.getElementById("creator-theme-selector").innerHTML = themesList;
+
 export {
     addList,
     addChapter,
